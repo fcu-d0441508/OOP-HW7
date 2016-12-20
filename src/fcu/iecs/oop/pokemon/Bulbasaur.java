99174d88a519ffec61022d8461878053a6d1fb75
d0441508 <a0930152994@gmail.com>
2016-12-21 03:16:13 +0800

    hw7

--- src/fcu/iecs/oop/pokemon/Bulbasaur.java
+++ src/fcu/iecs/oop/pokemon/Bulbasaur.java
@@ -1,5 +1,13 @@
 package fcu.iecs.oop.pokemon;
 
-public class Bulbasaur {
+public class Bulbasaur extends Pokemon {
+	public Bulbasaur(String name, PokemonType type, int cp) {
+		super(name, type, cp);
+	}
 
+	@Override
+	public void attack() {
+		System.out.println("Tackle¡K");
+		
+	}
 }
